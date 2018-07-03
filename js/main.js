971e2cb4bc68399ec672fe0a59ba54972a62ea12
Mishev <m.n.mishev@gmail.com>
2018-07-04 01:09:54 +0300

    fixed bugs

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 
-$.backstretch("../images/bg.jpg");
-
-var setActiveLink = $("#topNav").on("click","a",function(){
-    $("#topNav .active").toggleClass("active");
-    $this = $(this);
-    $this.toggleClass("active");
-    
-});
 
 
