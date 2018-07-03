f5a016954fce009d33d8ad531ab351f46967daa2
Mishev <m.n.mishev@gmail.com>
2018-07-04 01:06:55 +0300

    fixed bugs

--- js/main.js
+++ js/main.js
@@ -5,4 +5,7 @@ var setActiveLink = $("#topNav").on("click","a",function(){
     $("#topNav .active").toggleClass("active");
     $this = $(this);
     $this.toggleClass("active");
+    
+});
+
 
--- js/myApp.js
+++ js/myApp.js
@@ -1,3 +1,11 @@
+$.backstretch("../images/bg.jpg");
+
+var setActiveLink = $("#topNav").on("click","a",function(){
+    $("#topNav .active").toggleClass("active");
+    $this = $(this);
+    $this.toggleClass("active");
+
+});
 
 //Angular
 var myApp = angular.module('myApp', ['ngRoute']);
